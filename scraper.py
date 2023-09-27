e1448093437649745bce7c165b00fbac301554b3
vadim malinovsky <vadim.malinovsky@convexum.com>
2023-09-27 05:38:27 +0000

    a commit

--- scraper.py
+++ scraper.py
@@ -9,7 +9,6 @@ from rich import print
 from mongoengine import connect, StringField, IntField, EmbeddedDocument
 
 from handz import get_pricing_from_handz
-from models import CarAd, PriceHistory
 from normalize_json import normalize_json
 
 manufacturers_dict = {
@@ -125,21 +124,6 @@ def yad2_scrape(querystring: dict, last_page: int = 1):
     return car_ads_to_save
 
 
-def save_to_database(car_ads: list):
-    for car_details in car_ads:
-        car_ad = CarAd(id=car_details['id'])
-        car_ad.manufacturer = car_details['manufacturer']
-        car_ad.model = car_details['car_model']
-        car_ad.year = car_details['year']
-        car_ad.hand = car_details['hand']
-        car_ad.engine_size = car_details['engine_size']
-        car_ad.kilometers = car_details['kilometers']
-        car_ad.price = car_details['price']
-        car_ad.updated_at = car_details['updated_at']
-        car_ad.date_added = car_details['date_added']
-        for date_price in car_details['prices']:
-            car_ad.price_history.append(PriceHistory(price=date_price['price'], date=date_price['date']))
-        car_ad.save()
 
 
 def extract_car_details(feed_item: json):
