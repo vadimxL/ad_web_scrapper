0333381fae78fb4a293f158322d6953550274ed7
vadimv <vadim.malinovsky@convexum.com>
2024-04-18 18:35:31 +0300

    add gmail sender

--- gmail_sender/gmail_sender.py
+++ gmail_sender/gmail_sender.py
@@ -1,49 +1,86 @@
 import base64
 from email.message import EmailMessage
+import os
+from google.auth.transport.requests import Request
+from dotenv import load_dotenv
 
-import google.auth
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
+# load_dotenv()
+#
+# MY_ENV_VAR = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
+#
+# import google.auth
+# from googleapiclient.discovery import build
+# from googleapiclient.errors import HttpError
 
-def gmail_send_message():
-    """Create and send an email message
-  Print the returned  message id
-  Returns: Message object, including message id
+# If modifying these scopes, delete the file token.json.
+SCOPES = ["https://www.googleapis.com/auth/gmail.send"]
 
-  Load pre-authorized user credentials from the environment.
-  TODO(developer) - See https://developers.google.com/identity
-  for guides on implementing OAuth2 for the application.
-  """
-    creds, _ = google.auth.default()
 
-    try:
-        service = build("gmail", "v1", credentials=creds)
-        message = EmailMessage()
+class GmailSender:
+    def __init__(self):
+        self.creds = self._credentials()
 
-        message.set_content("This is automated draft mail")
+    @staticmethod
+    def _credentials():
+        creds = None
+        # The file token.json stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if os.path.exists("token.json"):
+            creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    "credentials.json", SCOPES
+                )
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open("token.json", "w") as token:
+                token.write(creds.to_json())
+        return creds
 
-        message["To"] = "vadimski30@gmail.com"
-        message["From"] = "malinvadim88@gmail.com"
-        message["Subject"] = "Automated draft"
+    def send(self, msg_content: str):
+        """
+        Create and send an email message
+        Print the returned  message id
+        Returns: Message object, including message id
+        """
+        try:
+            service = build("gmail", "v1", credentials=self.creds)
+            message = EmailMessage()
 
-        # encoded message
-        encoded_message = base64.urlsafe_b64encode(message.as_bytes()).decode()
+            message.set_content(msg_content)
 
-        create_message = {"raw": encoded_message}
-        # pylint: disable=E1101
-        send_message = (
-            service.users()
-            .messages()
-            .send(userId="me", body=create_message)
-            .execute()
-        )
-        print(f'Message Id: {send_message["id"]}')
-    except HttpError as error:
-        print(f"An error occurred: {error}")
-        send_message = None
-    return send_message
+            message["To"] = "vadimski30@gmail.com"
+            message["From"] = "malinvadim88@gmail.com"
+            message["Subject"] = "Automated draft"
+
+            # encoded message
+            encoded_message = base64.urlsafe_b64encode(message.as_bytes()).decode()
+
+            create_message = {"raw": encoded_message}
+            # pylint: disable=E1101
+            send_message = (
+                service.users()
+                .messages()
+                .send(userId="me", body=create_message)
+                .execute()
+            )
+            print(f'Message Id: {send_message["id"]}')
+        except HttpError as error:
+            print(f"An error occurred: {error}")
+            send_message = None
+        return send_message
 
 
 if __name__ == "__main__":
-    gmail_send_message()
+    gmail_sender = GmailSender()
+    gmail_sender.send("Hello, Vadim!")
